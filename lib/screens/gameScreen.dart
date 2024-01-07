828d52f0c922b9841d7743705874331210a74b74
Milan Maharjan <milanmaharjan2013@gmail.com>
2024-01-07 20:17:44 +0545

    commit

--- lib/screens/gameScreen.dart
+++ lib/screens/gameScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tictactoe/constants/colors.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class gameScreen extends StatefulWidget {
   const gameScreen({super.key});
@@ -10,6 +10,19 @@ class gameScreen extends StatefulWidget {
 }
 
 class _gameScreenState extends State<gameScreen> {
+  bool xTurn = true;
+  List<String> display = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
+  String result = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +41,14 @@ class _gameScreenState extends State<gameScreen> {
                           crossAxisCount: 3),
                       itemBuilder: ((context, index) {
                         return GestureDetector(
+                          onTap: () {
+                            _tapped(index);
+                          },
                           child: Container(
+                              child: Center(
+                                  child: Text(display[index],
+                                      style: GoogleFonts.lato(
+                                          color: Colors.white, fontSize: 69))),
                               decoration: BoxDecoration(
                                   color: colors.secondaryColor,
                                   borderRadius: BorderRadius.circular(20),
@@ -36,11 +56,99 @@ class _gameScreenState extends State<gameScreen> {
                                       width: 5, color: colors.primaryColor))),
                         );
                       }))),
-              Expanded(flex: 2, child: Text("Timer")),
+              Expanded(
+                  flex: 2,
+                  child: Text(
+                    result,
+                    style: GoogleFonts.lato(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w600),
+                  )),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _tapped(int index) {
+    setState(() {
+      if (xTurn && display[index] == '') {
+        display[index] = 'X';
+      } else if (!xTurn && display[index] == "") {
+        display[index] = 'O';
+      }
+      xTurn = !xTurn;
+      checkWinner();
+    });
+  }
+
+  void checkWinner() {
+    //check first row
+    if (display[0] == display[1] &&
+        display[0] == display[2] &&
+        display[0] != "") {
+      setState(() {
+        result = 'Player ' + display[0] + ' wins';
+      });
+    }
+
+    //check second row
+    if (display[3] == display[4] &&
+        display[3] == display[5] &&
+        display[3] != "") {
+      setState(() {
+        result = 'Player ' + display[3] + ' wins';
+      });
+    }
+    //check third row
+    if (display[6] == display[7] &&
+        display[6] == display[8] &&
+        display[6] != "") {
+      setState(() {
+        result = 'Player ' + display[3] + ' wins';
+      });
+    }
+    //check first column
+    if (display[0] == display[3] &&
+        display[0] == display[6] &&
+        display[0] != "") {
+      setState(() {
+        result = 'Player ' + display[0] + ' wins';
+      });
+    }
+    //check second column
+    if (display[1] == display[4] &&
+        display[1] == display[7] &&
+        display[1] != "") {
+      setState(() {
+        result = 'Player ' + display[1] + ' wins';
+      });
+    }
+    //check third column
+    if (display[2] == display[5] &&
+        display[2] == display[8] &&
+        display[2] != "") {
+      setState(() {
+        result = 'Player ' + display[2] + ' wins';
+      });
+    }
+    //check first diagonal
+    if (display[0] == display[4] &&
+        display[0] == display[8] &&
+        display[0] != "") {
+      setState(() {
+        result = 'Player ' + display[0] + ' wins';
+      });
+    }
+    //check second diagonal
+    if (display[2] == display[4] &&
+        display[2] == display[7] &&
+        display[2] != "") {
+      setState(() {
+        result = 'Player ' + display[2] + ' wins';
+      });
+    }
   }
 }
